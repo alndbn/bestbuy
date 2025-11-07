ce606845c2dddce997923e9e1eabf5b45b69ede4
Alena Drobner <alena.drobner@gmail.com>
2025-11-07 09:20:12 +0100

    Added comments to main.py

--- main.py
+++ main.py
@@ -3,23 +3,24 @@ from store import Store
 
 
 def main():
+    #create product objects
     bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
     mac = Product("MacBook Air M2", price=1450, quantity=100)
     pixel = Product("Google Pixel 7", price=500, quantity=250)
 
-    best_buy = Store([bose, mac, pixel])
+    #list for the store
+    product_list = [bose, mac, pixel]
 
+    #Store
+    best_buy = Store(product_list)
+
+    #Testcode
+    products = best_buy.get_all_products()
     print("Total items:", best_buy.get_total_quantity())
+    print("Order cost:", best_buy.order([(products[0], 1), (products[1], 2)]))
 
-    active_products = best_buy.get_all_products()
-    for product in active_products:
-        product.show()
-
-    total = best_buy.order([(bose, 5), (mac, 3)])
-    print("Order cost:", total)
-
-
-    print("\n--- Edge cases test---")
+    #Edge-Case-Test
+    print("\n--- Edge cases test ---")
     try:
         best_buy.order([(bose, 0)])
     except Exception as e:
